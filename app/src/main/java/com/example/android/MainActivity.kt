eb9520ffe3ace6deac8f4610f77591a9dda4e525
Anthony Bousser <anthony.bousser@epita.fr>
2022-01-12 12:33:59 +0100

    add second activity and Game Screen

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getGameScreen(view: View) {
+        val intent = Intent(this, GameActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
